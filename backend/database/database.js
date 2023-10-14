abd51682eb58044474f622c72632d3e9ed14474d
Juan Terneus <juanjose.terneus@verndale.com>
2023-10-13 20:29:08 -0500

    database

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres', 
   connection: {
-        host: env('DATABASE_HOST', 'private-db-postgresql-nyc3-48326-do-user-14552568-0.b.db.ondigitalocean.com'),
+        host: env('DATABASE_HOST', 'app-88060f10-32b4-4728-96db-c5b929ca5623-do-user-14552568-0.b.db.ondigitalocean.com'),
         port: env.int('DATABASE_PORT', 25060),
-        database: env('DATABASE_NAME', 'defaultdb'),
-        user: env('DATABASE_USERNAME', 'doadmin'),
-        password: env('DATABASE_PASSWORD', 'AVNS_xwwcig2gmMpEx5KaiPm'),
+        database: env('DATABASE_NAME', 'db'),
+        user: env('DATABASE_USERNAME', 'db'),
+        password: env('DATABASE_PASSWORD', 'AVNS_SbAMqmE0v6oGUCxKig7'),
         ssl: env.bool("DATABASE_SSL", true) && {
           rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
