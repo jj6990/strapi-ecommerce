e5ad612cc79fe3e80b6fdeb8d183218716c347e1
Juan Terneus <juanjose.terneus@verndale.com>
2023-10-15 11:31:43 -0500

    database

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'private-ecommerce-db-do-user-14552568-0.b.db.ondigitalocean.com'),
+      port: env.int('DATABASE_PORT', 25060),
+      database: env('DATABASE_NAME', 'defaultdb'),
+      user: env('DATABASE_USERNAME', 'doadmin'),
+      password: env('DATABASE_PASSWORD', 'AVNS__M9RCpU_vkehQekYFW8'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     useNullAsDefault: true,
   },
