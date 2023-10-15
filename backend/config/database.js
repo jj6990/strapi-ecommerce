fd1c05b6b86154613c6915cb194289f3fca963fb
Juan Terneus <juanjose.terneus@verndale.com>
2023-10-15 15:33:28 -0500

    database

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,15 +2,17 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ecommerce-db-do-user-14552568-0.b.db.ondigitalocean.com'),
+      host: env('DATABASE_HOST', 'db-postgresql-nyc1-32378-do-user-14552568-0.b.db.ondigitalocean.com'),
       port: env.int('DATABASE_PORT', 25060),
       database: env('DATABASE_NAME', 'defaultdb'),
       user: env('DATABASE_USERNAME', 'doadmin'),
-      password: env('DATABASE_PASSWORD', 'AVNS__M9RCpU_vkehQekYFW8'),
-      ssl: env.bool('DATABASE_SSL', false),
+      password: env('DATABASE_PASSWORD', 'AVNS_CnriwfIEYiDiTqW6eD5'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
